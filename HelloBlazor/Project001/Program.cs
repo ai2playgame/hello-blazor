9a7f602f7d61a0b5681eb6412afd862e6a47b0b3
ai2playgame <detective9684@gmail.com>
2024-05-09 09:16:05 +0900

    add R3 package

--- HelloBlazor/Project001/Program.cs
+++ HelloBlazor/Project001/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Project001;
+using R3;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
