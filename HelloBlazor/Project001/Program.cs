9e2e6e803df0785b4c23ba2146a483fb65462a7b
ai2playgame <detective9684@gmail.com>
2024-05-10 08:18:18 +0900

    R3.ReactivePropertyを活用した値更新検知の仕組みを実装

--- HelloBlazor/Project001/Program.cs
+++ HelloBlazor/Project001/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Project001;
-using R3;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// DIに登録
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new StateService());
 
 await builder.Build().RunAsync();
---
+++ HelloBlazor/Project001/StateService.cs
@@ -0,0 +1,8 @@
+using R3;
+
+namespace Project001;
+
+public class StateService
+{
+	public ReactiveProperty<int> CurrentCount = new(0);
+}
